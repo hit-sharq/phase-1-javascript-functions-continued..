2f5c9350a48641236bb169874bc9cb548726e946
joshua mwendwa <leeroyjoshu087@gmail.com>
2024-03-24 12:18:04 +0300

    initial commit

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
 // code your solution here
+function saturdayFun(activity= "roller-skate"){
+    return `This Saturday, I want to ${activity}!`
+    }
+    function mondayWork(activity= "go to the office") {
+        return `This Monday, I will ${activity}.`
+    }
+    function wrapAdjective(_highlight="*") { 
+        return function (_message="special"){
+            return `You are ${_highlight}${_message}${_highlight}!`
+        }
+    }
+    
